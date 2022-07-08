c29c20b079f6258bfc0c3d0d57e9215806554d48
Larry Applegate <larry@rivergatesoftware.com>
2022-07-08 16:58:27 -0600

    Initial Commit

---
+++ Simple/SimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleApp.swift
+//  Simple
+//
+//  Created by Lawrence G Applegate on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
